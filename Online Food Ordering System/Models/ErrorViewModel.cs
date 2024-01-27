2433dcdb8e72424017d2cef73d3248bdc8eecd98
Yossef Sayed Elgohl <Yossef Elgohl>
2024-01-27 09:43:44 +0200

    Add project files.

---
+++ Online Food Ordering System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Online_Food_Ordering_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
