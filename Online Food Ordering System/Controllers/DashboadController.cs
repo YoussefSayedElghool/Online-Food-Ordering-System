617f67459a4baf4c17a820e2881132898efb181a
YoussefSayedElghool <YoussefSayedElghool>
2024-06-15 12:28:59 +0300

    Create "Dashboard statics.psd" , that repersent Dashboard Layout and show some expected contant of "statics" page
    Apply "Dashboard statics.psd" at our view
     1- Create "_AsideOprationPartial.cshtml"
          partial view and it's css file is "_AsideOprationPartial.css"
    
    2- Use "_AsideOprationPartial"  partial view at "_AsidePartial"
         intead of "button"
    
    3- Add logo and brand name at "_AsidePartial"
    
    4-  Create "_DashboadHeaderPartial" partial view and use it at "_DashboardLayout"
    
    5- Add all required stlye sheet at  "_DashboardLayout"
    
    Create new foler at "Imges" folder name "Assts" that should contain
    web site Asstes. To separate the web site statice contant from the food imge
    that will added in the future. Also should other folder to be added it's name is "Food"  to contain the Food imge only
    
    Create new folder at "Views" folder named "Dashboard"
    "Dashboard" folder has 3 file
    - FoodEdit.cshtml
    - Orders.cshtml
    - Statistics.cshtml
    And Create "DashboadController.cs" that route to the pervouse pages

---
+++ Online Food Ordering System/Controllers/DashboadController.cs
@@ -0,0 +1,39 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Online_Food_Ordering_System.Models;
+using Online_Food_Ordering_System.Service.abstraction_layer;
+using Online_Food_Ordering_System.View_Models;
+
+namespace Online_Food_Ordering_System.Controllers
+{
+    public class DashboadController : Controller
+    {
+
+        public DashboadController()
+        {
+        }
+
+
+        [HttpGet]
+        public IActionResult Statistics()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Orders()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult FoodEdit()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ Online Food Ordering System/View Models/DashboadOprationViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Online_Food_Ordering_System.Models
+{
+    public class DashboadOpration
+    {
+        public string Title { get; set; }
+        public string Imge { get; set; }
+    }
+}
