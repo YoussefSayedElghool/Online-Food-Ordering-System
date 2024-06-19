b9335b8a8960fcfa220eae1627eb1266c5aa79ab
YoussefSayedElghool <YoussefSayedElghool>
2024-06-19 14:14:27 +0300

    Checkout partially work
    Create  "Checkout" folder at "Views" folder
    
    Create new Controller name "CheckoutController" has two Actions
        - Checkout Get varbe version
            - route use to "Checkout.cshtml" that exist at "Checkout" folder
        - Checkout Post varbe
            - used to Make order

---
+++ Online Food Ordering System/Controllers/CheckoutController.cs
@@ -0,0 +1,45 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Online_Food_Ordering_System.Models;
+using Online_Food_Ordering_System.Repository;
+using Online_Food_Ordering_System.Repository.abstraction_layer;
+using Online_Food_Ordering_System.Service.abstraction_layer;
+using Online_Food_Ordering_System.View_Models;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Security.Claims;
+
+namespace Online_Food_Ordering_System.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly ICartService cartService;
+        private readonly ICheckoutRepository checkoutRepository;
+
+        public CheckoutController(ICartService cartService , ICheckoutRepository checkoutRepository)
+        {
+            this.cartService = cartService;
+            this.checkoutRepository = checkoutRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(string paymentMethod)
+        {
+            if (paymentMethod == "offline")
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                checkoutRepository.MakeOrder(userId);
+            }
+            return View();
+        }
+
+    }
+}
